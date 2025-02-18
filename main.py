0e76a322c94a6b1ebaa536b72c54cb03ce895b02
AMaldonado1 <a100maldo983@outlook.com>
2025-02-18 01:24:28 -0500

    added SQL to create database/tables and added menu loop

--- main.py
+++ main.py
@@ -4,12 +4,98 @@ import pymysql
 import getpass
 
 
-def delete_database():
+DATABASE_NAME = "school"
+FIND_DATABASE = (f"{DATABASE_NAME}",)
+TABLE_NAMES = ("student", "instructor", "course", "enrollment")
+FIND_TABLES = ((f"{TABLE_NAMES[0]}",), (f"{TABLE_NAMES[1]}",), (f"{TABLE_NAMES[2]}",), (f"{TABLE_NAMES[3]}",))
+CREATE_TABLES = {f"{TABLE_NAMES[0]}": f"""CREATE TABLE {TABLE_NAMES[0]}
+    (student_id     VARCHAR(10), 
+	 name           VARCHAR(20), 
+	 credits        NUMERIC(3,0) CHECK (credits > 0),
+	 PRIMARY KEY (student_id)
+	);""",
+                 f"{TABLE_NAMES[1]}": f"""CREATE TABLE {TABLE_NAMES[1]}
+    (instructor_id      VARCHAR(10), 
+	 name               VARCHAR(20), 
+	 department         VARCHAR(20),
+	 PRIMARY KEY (instructor_id)
+	);""",
+                 f"{TABLE_NAMES[2]}": f"""CREATE TABLE {TABLE_NAMES[2]}
+    (course_id          VARCHAR(10), 
+	 instructor_id      VARCHAR(10), 
+	 name               VARCHAR(20),
+	 credits            NUMERIC(3,0) CHECK (credits > 0),
+	 PRIMARY KEY (course_id),
+	 FOREIGN KEY (instructor_id) REFERENCES {TABLE_NAMES[1]}(instructor_id)
+	    ON DELETE SET null
+	);""",
+                 f"{TABLE_NAMES[3]}": f"""CREATE TABLE {TABLE_NAMES[3]}
+    (course_id          VARCHAR(10), 
+	 student_id         VARCHAR(10), 
+	 semester           ENUM('Winter', 'Spring', 'Summer', 'Fall'),
+	 year               YEAR,
+	 PRIMARY KEY (course_id, student_id),
+	 FOREIGN KEY (course_id) REFERENCES {TABLE_NAMES[2]}(course_id)
+	    ON DELETE CASCADE,
+	 FOREIGN KEY (student_id) REFERENCES {TABLE_NAMES[0]}(student_id)
+	    ON DELETE CASCADE	 
+	);"""}
+
+
+def view_table():
+    print("Table")
     pass
 
 
-def create_database():
-    pass
+def add_entry():
+    print("Adding new entry/row")
+
+
+def remove_entry():
+    print("Removing old entry/row")
+
+
+def delete_all():
+    print("Deleting all entries/rows")
+
+
+def add_tables(cursor):
+    print("\nChecking if all Tables already exist...")
+    try:
+        cursor.execute("SHOW TABLES;")
+        all_tables = cursor.fetchall()
+        for table in FIND_TABLES:
+            if table in all_tables:
+                print(f"\"{table[0]}\" table already exists.")
+            else:
+                create_table = CREATE_TABLES[table[0]]
+                cursor.execute(create_table)
+                print(f"\"{table[0]}\" table has been created!")
+        return True
+
+    except pymysql.err.OperationalError:
+        print(f"Unable to create Tables.")
+        return False
+
+
+def create_database(cursor):
+    print(f"Checking if \"{DATABASE_NAME}\" database already exists...")
+    try:
+        cursor.execute("SHOW DATABASES;")
+        all_databases = cursor.fetchall()
+        if FIND_DATABASE in all_databases:
+            print(f"\"{DATABASE_NAME}\" database already exists.")
+        else:
+            create_school_database = f"CREATE DATABASE {DATABASE_NAME}"
+            cursor.execute(create_school_database)
+            print(f"\"{DATABASE_NAME}\" database has been created!")
+        select_school_database = "USE school;"
+        cursor.execute(select_school_database)
+        return True
+
+    except pymysql.err.OperationalError:
+        print(f"Unable to create \"{DATABASE_NAME}\" database.")
+        return False
 
 
 def create_connection():
@@ -27,29 +113,71 @@ def create_connection():
             host=mysql_host,
             port=mysql_port
         )
-        print("Successfully connected to the MySQL database!")
+        print("\nSuccessfully connected to the MySQL database!\n")
         return server_connection
 
     except pymysql.err.OperationalError:
-        print("Unable to connect to MySQL database! Double check your credentials.")
+        print("\nUnable to connect to MySQL database. Double check your credentials.\n")
         return None
 
 
-def run_crud_app():
-    pass
+def main_menu(cursor):
+    menu = """
+    Please choose an option:
+    1. View tables
+    2. Add entry
+    3. Remove entry
+    4. Delete all entries
+    5. Exit database
+    """
+    print(menu)
+    command = input(">> ")
+    match command:
+        case "1":
+            view_table()
+        case "2":
+            add_entry()
+        case "3":
+            remove_entry()
+        case "4":
+            delete_all()
+        case "5":
+            return True
+        case _:
+            print("Invalid option.")
+
+    return False
+
+
+def run_crud_app(cursor):
+    # Check if school database already exists, if not make it
+    if not create_database(cursor):
+        return
+    # Check if school tables already exists, if not make them
+    if not add_tables(cursor):
+        return
+    # Main loop
+    exit_database = False
+    while not exit_database:
+        exit_database = main_menu(cursor)
+    # Exiting message
+    print("Thank you for using School CRUD MySQL")
+    return
 
 
 def main():
+    print("CMSC 447 Assignment 1 Part 2: School CRUD MySQL\n")
     # Attempt to create a connection
     mysql_server = create_connection()
     # Check if connection is successful
     if mysql_server is None:
         return
-
+    # Create cursor for SQL
     cursor = mysql_server.cursor()
+    # Simple CLI CRUD
+    run_crud_app(cursor)
+    # Close the MySQL server
     mysql_server.close()
-    # Check if school database already exists, if so use it
-
 
 
 if __name__ == "__main__":
